8df563c3ba04400daddb867c15938843b9d4f206
Lazar Mrkic <lazar.mrkic@sigmoid.rs>
2024-03-25 13:39:53 +0100

    fix variable name typo

--- index.test.ts
+++ index.test.ts
@@ -3,7 +3,7 @@ import { describe, test, expect } from "@jest/globals"
 
 describe("Test cases for leap year function", ()=> {
     const leapYears = [2000, 2008, 2012, 2016]
-    const nonLeapYearsDivisiblyBy100Not400 = [1700, 1800, 1900, 2100]
+    const nonLeapYearsDivisibleBy100Not400 = [1700, 1800, 1900, 2100]
     const nonLeapYears = [2017, 2018, 2019]
 
     leapYears.forEach((year) => {
@@ -12,7 +12,7 @@ describe("Test cases for leap year function", ()=> {
         })
     });
 
-    nonLeapYearsDivisiblyBy100Not400.forEach((year) => {
+    nonLeapYearsDivisibleBy100Not400.forEach((year) => {
         test(`${year} is not a leap year (divisible by 100, but not by 400)`, ()=> {
             expect(isLeapYear(year)).toEqual(false)
         });
